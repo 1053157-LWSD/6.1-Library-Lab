30a8132923b5ebc417af7a371eb05228e01cfd86
Anusha <1053157@lwsd.org>
2025-12-17 18:37:47 -0800

    make library lab for students in new repo

---
+++ Library.java
@@ -0,0 +1,35 @@
+/* 
+    Prompt:
+
+Write a method, readBook, which prompts the user to enter a file name 
+and then checks if the given file exists in the repository. 
+If the file exists, the method should read the file by opening it and displaying the text. (read the file)
+If the file does not exist, handle the error by informing the user and ask them to enter another 
+file name until a valid file name is provided. *Remember to add any exceptions.
+Precondition: Assume user always adds file type at end of file name.
+
+    Example Ouput Scenario:
+
+Welcome to the Library!
+Type the file name of the book you want to read: notHere.txt
+The file you are looking for isn't found.
+Type the file name of the book you want to read: isHere.txt
+File Contents of Book:
+Testing 1, 2, 3
+
+*/
+
+class Library {
+    public static void main(String[] args)  {
+        // Add code here in main if necessary
+        System.out.println("Welcome to the Library!");
+        System.out.print("Type in the file name of the book you want to read: ");
+        readBook();
+    }
+
+public static void readBook() {
+
+    // Add your code here
+
+    }
+}
